a81d4ea1369fa153a415841dd3c18bec57299623
Saifullah <saifullahkhaliq@gmail.com>
2021-05-09 18:38:31 +0500

    Added ScatterAnimate.py

--- Postprocessor/ScatterAnimate.py
+++ Postprocessor/ScatterAnimate.py
@@ -9,14 +9,15 @@ from os import walk
 import glob
 import fnmatch
 import numpy as np
-
+#
 # path = os.getcwd() + '/sample_output'
-path = sys.argv[1]
-df_name = []
-df_list = []
+# print(path)
+# # path = sys.argv[1]
+# df_name = []
+# df_list = []
 # for root, dirs, files in os.walk(path, topdown=False):
 #     for name in files:
-#         if "covid_out_infections.csv" in name:
+#         if "covid_out_infections_small.csv" in name:
 #             filepath = os.path.join(root, name)
 #             df = pd.read_csv(filepath)
 #             columns = ["time",  "x", "y", "type"]
@@ -24,42 +25,78 @@ df_list = []
 #             for column in df:
 #                 df_name.append(column)
 #             df_list.append(df)
-df = pd.read_csv(path)
+# # df = pd.read_csv(path)
+# # columns = ["time",  "x", "y", "location_type"]
+# # df.columns = columns
+# # for column in df:
+# #     df_name.append(column)
+# # df_list.append(df)
+# df = pd.concat(df_list, axis=1, ignore_index=True)
+# df.columns = df_name
+# # df = df[(df['time']>0) & (df['time']<=414)]
+# df = df[(df['time']>0)]
+
+
+df_name = []
+df_list = []
+df = pd.read_csv('sample_output/covid_out_infections.csv')
 columns = ["time",  "x", "y", "type"]
 df.columns = columns
 for column in df:
     df_name.append(column)
 df_list.append(df)
+
+
+
+
 df = pd.concat(df_list, axis=1, ignore_index=True)
 df.columns = df_name
 # df = df[(df['time']>0) & (df['time']<=414)]
 df = df[(df['time']>0)]
+
+
+
 
 #create dataframe for each day
 cdf = pd.DataFrame(columns = ["time",  "x", "y", "type"])
 frames = []
 grouped = df.groupby('time')
 for group in grouped:
-    day = group[0]
-    data = df[(df['time']<=day)]
-    data['time'] = day
-    # print(day, "  ", len(group[1]), "  ", len(data))
-    cdf = cdf.append(data, ignore_index = True)
+      day = group[0]
+      data = df[(df['time']<=day)]
+      data['time'] = day
+      print(day, "  ", len(group[1]), "  ", len(data))
+      cdf = cdf.append(data, ignore_index = True)
 
 x = cdf['x'].mean()
 y = cdf['y'].mean()
 print(x,y)
 cdf['size'] = 1
 
+# cdf2 = cdf[(cdf['time']<=20)]
+# cdf2['type'].unique()
+
+# color_discrete_map = {'house': px.colors.qualitative.G10[1],
+#                       'office': px.colors.qualitative.G10[5],
+#                       'shopping': px.colors.qualitative.G10[2],
+#                       'school': px.colors.qualitative.G10[4],
+#                       'leisure': px.colors.qualitative.G10[3],
+#                       'park': px.colors.qualitative.G10[7],
+#                       'hospital': px.colors.qualitative.G10[6],
+#                       'traffic': px.colors.qualitative.G10[0],
+#                       'place_of_worship': px.colors.qualitative.G10[8]}
+
 color_discrete_map = {'house': px.colors.qualitative.G10[1],
-                      'office': px.colors.qualitative.G10[5],
-                      'shopping': px.colors.qualitative.G10[2],
+                      'office': px.colors.qualitative.G10[2],
+                      'shopping': px.colors.qualitative.G10[3],
                       'school': px.colors.qualitative.G10[4],
-                      'leisure': px.colors.qualitative.G10[3],
-                      'park': px.colors.qualitative.G10[7],
-                      'hospital': px.colors.qualitative.G10[6],
-                      'traffic': px.colors.qualitative.G10[0],
-                      'place_of_worship': px.colors.qualitative.G10[8]}
+                      'leisure': px.colors.qualitative.G10[5],
+                      'park': px.colors.qualitative.G10[6],
+                      'hospital': px.colors.qualitative.G10[7],
+                      'traffic': px.colors.qualitative.G10[8],
+                      'place_of_worship': px.colors.qualitative.G10[9]}
+
+
 fig = px.scatter_mapbox(
     cdf, lat="y", lon="x",
     size= "size",
@@ -69,9 +106,40 @@ fig = px.scatter_mapbox(
     hover_name="type",
     animation_frame="time", animation_group="time",
 )
+
+
+# fig.layout.updatemenus[0].buttons[0].args[1]["frame"]["duration"] = 200
+# fig.layout.updatemenus[0].buttons[0].args[1]["transition"]["duration"] = 200
+# # fig.layout.coloraxis.showscale = True
+# fig.layout.sliders[0].pad.t = 0
+# fig.layout.sliders[0].x = 0
+# fig.layout.sliders[0].y = -0.1
+# fig.layout.updatemenus[0].pad.t= 0
+# fig["layout"].pop("updatemenus")
+# fig.layout.mapbox={
+#                       'accesstoken':'pk.eyJ1IjoiaW1haG1vb2QiLCJhIjoiY2tjdnE0MDBjMDZuYjJ6cXY2aGE4OWN2aCJ9.bRfBBl_o-iRCv90oKtqdsA',
+#                       'center':{"lat": y, "lon":x},
+#                       'zoom':11,
+#                       'style':'open-street-map'
+#                   }
+# fig.update_layout(
+#     title='Islamabad - Locationwise Infected Cases per day',
+#     width = 1024,
+#     height = 600,
+#     legend=dict(
+#         title = 'Building Type',
+#         orientation="h",
+#         y=0,
+#         x=0,
+#         traceorder='reversed'
+#     )
+# )
+# py.offline.plot(fig, filename='scatter_animate.html')
+# fig.show()
+
 fig.layout.updatemenus[0].buttons[0].args[1]["frame"]["duration"] = 200
 fig.layout.updatemenus[0].buttons[0].args[1]["transition"]["duration"] = 200
-# fig.layout.coloraxis.showscale = True
+fig.layout.coloraxis.showscale = True
 fig.layout.sliders[0].pad.t = 0
 fig.layout.sliders[0].x = 0
 fig.layout.sliders[0].y = -0.1
@@ -79,13 +147,15 @@ fig.layout.updatemenus[0].pad.t= 0
 fig["layout"].pop("updatemenus")
 fig.layout.mapbox={
                       'accesstoken':'pk.eyJ1IjoiaW1haG1vb2QiLCJhIjoiY2tjdnE0MDBjMDZuYjJ6cXY2aGE4OWN2aCJ9.bRfBBl_o-iRCv90oKtqdsA',
-                      'center':{"lat": y, "lon":x},
+                      'center':{"lat":y, "lon":x},
                       'zoom':11,
                       'style':'open-street-map'
                   }
+
+
 fig.update_layout(
     title='Islamabad - Locationwise Infected Cases per day',
-    width = 1024,
+    width = 1280,
     height = 600,
     legend=dict(
         title = 'Building Type',
--- Preprocessor/README.md
+++ Preprocessor/README.md
@@ -11,7 +11,7 @@ The OSM files (format osm.pbf) used for this project are already added in this r
 4) Get its id, which is of type 'relation' (combination of nodes and points)
 5) To extract polygon (boundary) go to http://polygons.openstreetmap.fr/ and enter id, and download file
 6) Save it with name .poly at the same location where Pakistan OSM is saved
-7) Now you have your region boundries and we want to  extract it from Pakistan osm
+7) Now you have your region boundaries and we want to  extract it from Pakistan osm
 8) Use the provided tool osmconvert.exe and type the following command `osmconvert <parent osm> -B="<region polygon file>" --complete-ways --complete-multipolygons -o=<output filename>`
 9) eg. `osmconvert pakistan.osm -B="islamabad.poly" --complete-ways --complete-multipolygons -o=islamabad.osm.pbf`
 10) The osm_parser.py accepts .osm.pbf files. In case you have osm files you can convert them to .pbf format using osmconvert as well.
@@ -21,14 +21,14 @@ The OSM files (format osm.pbf) used for this project are already added in this r
 ## Installing Requirements
 To install requirements run the following command.  
 `pip3 install -r requirements.txt` 
-Please note that you need to create a conda enviroment first with python3.8 or above before installing requirements or executing code.  
-The soruce code has been executed and tested on ubunut OS with a conda environment. It may or may not run on Windows systems.  
+Please note that you need to create a conda environment first with python3.8 or above before installing requirements or executing code.  
+The soruce code has been executed and tested on ubuntu OS with a conda environment. It may or may not run on Windows systems.  
 
 ## Extracting locations
 osm_parser.py parses input osm.pbf file, extracts variety of places and outputs a location graph. 
 The script accepts multiple parameters as input
 1) --input <path to osm.pbf file to parse> : if you skip it the script will use a default osm file  
-2) --population: population of a specifc region
+2) --population: population of a specific region
 3) --leisure: to extract leisure places  
 4) --schools: to extract schools  
 5) --hospitals: to extract hospitals  
